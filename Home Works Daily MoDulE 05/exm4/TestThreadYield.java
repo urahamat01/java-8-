409dc4bd76c3f68f64684cf7850ec654d93ce163
MD. RAHAMAT ULLAH <urahamat01@gmail.com>
2019-02-10 02:59:47 -0800

    Class Works

--- Home Works Daily MoDulE 05/exm4/TestThreadYield.java
+++ Home Works Daily MoDulE 05/exm4/TestThreadYield.java
@@ -1,30 +1,32 @@
 
 
 
-package com.coderbd.book.threads.ex4;
-
-
-
-
 public class TestThreadYield implements Runnable {
 
     Thread t;
 
     TestThreadYield(String str) {
+		
         t = new Thread(this, str);
+		
         // this will call run() function
+		
         t.start();
     }
 
     @Override
     public void run() {
+		
         for (int i = 0; i < 20; i++) {
+			
             // yields control to another thread every 5 iterations
+			
             if ((i % 5) == 0) {
-                System.out.println(Thread.currentThread().getName()
-                        + " yielding control...");
+                System.out.println(Thread.currentThread().getName()+ "yielding control...");
+				
                 /* causes the currently executing thread object to temporarily 
             pause and allow other threads to execute */
+			
                 Thread.yield();
             }
         }
